588ca32d5486f9895447186f2c47402a98c4ef86
Bello Damilola Samuel <samuel@astract.com.ng>
2021-05-20 21:14:46 +0100

    quick search engine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react';
+
+const a = {
+  user: {
+    name: {
+      firstName: "James",
+      lastName: "Ibori"
+    },  
+    location: {
+      city: "Ikoyi",
+      state: "Lagos",
+      address: "One expensive house like that"
+    },
+    work: {
+      firstJob: "Developer",
+      seconJob: "Teaching",
+    },
+    hobbies: {
+      firstHobby: 'Games',
+      secondHobby: 'Sleep',
+    }
+  }
+}
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+  const [location, setLocation] = useState('');
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  }
+  const handleSearch = () => {
+    var location = "Not found";
+    for(let key in a.user){
+      const obj = a.user[key];
+      for (let item in obj){
+        if(obj[item].toLowerCase() === searchInput.toLowerCase()) {
+          location = `a.user.${key}.${item}`
+          break;
+        }
+      }
+    }
+    setLocation(location)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="searchBox">
+        <input 
+          className="searchInput" 
+          type="text" 
+          placeholder="Search" 
+          value={searchInput}
+          onChange={handleChange}
+        />
+        <button className="searchButton" onClick={handleSearch}>
+            Search
+        </button>
+      </div>
+      { location && <p className="searchResult">Search Result: {location}</p>}
     </div>
+    
   );
 }
 
